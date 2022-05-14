1c586eac59631b3b39e00189b2b85b53d6d75251
dbalazs95 <dbalazs9511@gmail.com>
2022-05-14 11:09:09 +0200

    tesztek functionjai elkezdése

--- teszt.js
+++ teszt.js
@@ -1,34 +1,45 @@
-window.addEventListener('load', init)
+window.addEventListener("load", init);
 function $(elem) {
-  return document.querySelectorAll(elem)
+  return document.querySelectorAll(elem);
 }
-
-const alt_kerdesek = [];
-const kozep_kerdesek = [];
-const felnott_kerdesek = [];
+var valasztas = ";"
+const kerdesek = [];
 
 function init() {
-    beolvas()
-    }
+    valaszt();
+  beolvas(kerdesek, valasztas);
+}
 
-function beolvas(){
-    fetch("tesztkerdesek.json")
-        .then((res) => res.json())
-        .then((data) => {
-            data.alt_isk.forEach((elem) => {
-                 alt_kerdesek.push(elem);
-            data.kozep_isk.forEach((elem) => {
-                kozep_kerdesek.push(elem);
-            data.felnott.forEach((elem) => {
-                felnott_kerdesek.push(elem);
-            })
-            })
-             });
-            console.log(alt_kerdesek)
-            console.log(kozep_kerdesek)
-            console.log(felnott_kerdesek)
-        })
-        .catch((err) => {
-            console.log(err);
-        })
+function beolvas(tomb, kulcs) {
+  fetch("tesztkerdesek.json")
+    .then((res) => res.json())
+    .then((data) => {
+      data[kulcs].forEach((elem) => {
+        tomb.push(elem);
+      });
+      console.log(tomb);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+function valaszt(){
+    var gombok = document.querySelectorAll('button');
+    console.log(gombok)
+    gombok.onclick = function() {
+        gombok.forEach(function(gomb){
+            if (gomb.getelementbyID.includes("alt")) {
+                valasztas = "alt_isk"
+                return valasztas
+            }
+            else if (gomb.getelementbyID.includes("kozep")) {
+                valasztas = "kozep_isk"
+                return valasztas
+            }
+            else if (gomb.getelementbyID.includes("felnott")) {
+                valasztas = "felnott"
+                return valasztas
+            }
+    })
+}
 }
