5416cade45faff99760678073c9426277b6a9e1d
dbalazs95 <dbalazs9511@gmail.com>
2022-05-20 19:37:11 +0200

    úgy ahogy műlödik, nem értékel jól

--- tesztek/teszt.js
+++ tesztek/teszt.js
@@ -3,9 +3,11 @@ function $(elem) {
   return document.querySelectorAll(elem);
 }
 
-
+var i = 0;
 const kerdesek = [];
 var txt = ""
+
+
 function init() {
   var valasztas = "";
   var gombok = document.getElementsByClassName('button');
@@ -13,27 +15,6 @@ function init() {
   for (let index = 0; index < gomb.length; index++) {
       gomb[index].addEventListener("click", kattint);
   }
-
-  //teszt(kerdesek)
-}
-
-function teszt(tomb){
-    console.log(tomb);
-    var txt = '<form id="teszt>'
-    tomb.forEach(function (kerdes){
-      console.log(kerdes)
-      for (const key in kerdes) {
-        if (key.includes("kerdes")) {
-          txt += `<h3 id="kerdes">${kerdes[key]}</h3>`
-        }else if (key.includes("valasz")) {
-          txt += `<input id="valasz" type="radio" name="1" value="${key}">${kerdes[key]}<br>`
-        }else if (key.includes("helyes")) {
-          txt+= `<input id="valasz" type="radio" name="1" value="${key}">${kerdes[key]}<br>`
-        }       
-      }
-      txt += '<input type="submit" value="Submit"></form>'
-      $("article")[0].innerHTML = txt
-    })
 }
 
 function kattint(event){
@@ -41,36 +22,48 @@ function kattint(event){
   if (event.target.nodeName === "BUTTON") {
     valasztas = event.target.id
   }
-console.log(valasztas)
 
-let kerdes = "tesztkerdesek.json"
-  fetch(kerdes)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data)
-      data[valasztas].forEach((elem) => {
-        kerdesek.push(elem);
-      });
-      console.log(kerdesek);
-      txt += '<div><form id="teszt>'
-    console
-    kerdesek.forEach(function (kerdes){
-      console.log(kerdes)
-      for (const key in kerdes) {
-        if (key.includes("kerdes")) {
-          txt += `<h3 id="kerdes">${kerdes[key]}</h3>`
-        }else if (key.includes("valasz")) {
-          txt += `<input id="valasz" type="radio" name="1" value="${key}">${kerdes[key]}<br>`
+  let kerdes = "tesztkerdesek.json"
+    fetch(kerdes)
+      .then((res) => res.json())
+      .then((data) => {
+        data[valasztas].forEach((elem) => {
+          kerdesek.push(elem);
+        });
+        txt += '<div><form id="teszt>'
+      kerdesek.forEach(function (kerdes){
+        
+        for (const key in kerdes) {
+          if (key.includes("kerdes")) {
+            txt += `<h3 id="kerdes">${kerdes[key]}</h3><br>`
+            i++;
+          }else if (key.includes("valasz")) {
+            txt += `<input id="valasz" type="radio" name="${i}.kerdes" value="${key}">${kerdes[key]}<br>`
+          }
         }
-      }
-      
-    })
-    txt += '<input type="submit" value="Submit"></form></div>'
-      $("article")[0].innerHTML = txt
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+        
+      })
+      txt += '<button onclick="return kiertekel()">Értékelés</button></form></div>'
+        $("article")[0].innerHTML = txt
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-     
+      
+}
+
+function kiertekel() {
+  var joValasz = 0;
+for (var index = 0; index < kerdesek.length; index++) {
+    var radios = document.getElementsByName(i+"kerdes");
+    for (var j = 0; j < radios.length; j++) {
+      var radio = radios[j];
+      console.log(radio)
+      if (radio.value == kerdesek[index][4] && radio.checked) {
+        joValasz ++;
+      }
+    }
+  }
+  alert("Jó válaszok: "+joValasz);
 }
